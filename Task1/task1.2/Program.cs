0e5e198ef0fcaf2e1e222cf390aedb1459fb1d46
Artem <kos107@bk.ru>
2024-03-21 19:08:49 +0300

    нахождение 1 и 2 чила

--- Task1/task1.2/Program.cs
+++ Task1/task1.2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿System.Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    System.Console.WriteLine("Число трехзначное! ");
+
+    int left = num / 100;
+    int rig = num % 10;
+    int sum = left + rig;
+    System.Console.WriteLine(num + " => " + sum);
+}
+else
+{
+    System.Console.WriteLine("число не трехзначное");
+}
