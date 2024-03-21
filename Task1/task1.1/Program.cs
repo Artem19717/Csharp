12422cbe4c252e68bcc68f1910f4a5236572f1d5
Artem <kos107@bk.ru>
2024-03-21 15:19:21 +0300

    на вход целое число

--- Task1/task1.1/Program.cs
+++ Task1/task1.1/Program.cs
@@ -1,9 +1,13 @@
-﻿System.Console.WriteLine("введите число");
+﻿Console.WriteLine("введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-int i = -N;
-while (i <= N)
-{
+// int i = -N;
+// while (i <= N)
+// {
+//     System.Console.Write(i + " ");
+//     i++;
+// }
+
+for (int i = -N; i <= N; i++)
     System.Console.Write(i + " ");
-    i++;
-}
+
