a793db2866dc5d5ce7bc0115df333abb0ee48b03
Artem <kos107@bk.ru>
2024-03-21 12:26:53 +0300

    корень задвча

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    System.Console.WriteLine("a = " + num1 +", b = " + num2 + "=> да");
+}
+else
+{
+    System.Console.WriteLine($"a = {num1}, b = {num2} => нет");
+}
+
